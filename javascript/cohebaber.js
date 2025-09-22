4bfda9ca587689e6e234d9852caaa58dc4476a9e
naveen6010 <naveen972821@gmail.com>
2025-09-22 15:36:29 +0530

    learn

---
+++ javascript/cohebaber.js
@@ -0,0 +1,35 @@
+/*console.log("naveen");
+// do while loop..
+let i=1;
+do{
+    console.log("naveen");
+    i++;
+
+}while(i<10);
+// do while loop is atleast one time is execute no matter the condition is true or or false..
+// its does't check the condition on ist time run.
+let name=`naveen sharma`;
+console.log(name);
+// we easily add space using bactics(``)..
+let firstname=new String("naveen sharma");
+console.log(firstname);
+/*
+operation{
+1:concatenation
+2:substring
+3:length
+4:upppercase
+5:lowercase
+6:charAT
+7:indexof
+}*/
+// bactics (template literals);.............
+/* let opt1='naveen ';
+let opt2='sharma ';
+let ans = opt1  +  opt2;
+console.log(ans);
+let finalans=`${opt1}${opt2}`;
+console.log(finalans.length);
+let hlo="ke\\ haal\\ chal\\ chore";
+let word=hlo.split('\\');
+console.log(word);*/
