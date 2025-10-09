fcfb18f4333cdb49e5eb271bdcfbefc7a1f89eda
naveen6010 <naveen972821@gmail.com>
2025-10-09 11:59:44 +0530

    push

--- javascript/cohebaber.js
+++ javascript/cohebaber.js
@@ -289,3 +289,38 @@ array.forEach(element => {
     console.log(array.length);
 });
 */
+/* let n = 5; // height of the pyramid
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+
+  // Print leading spaces
+  for (let j = 0; j < i; j++) {
+    row += " ";
+  }
+
+  // Print stars
+  for (let k = 0; k < 2 * (n - i) - 1; k++) {
+    row += "*";
+  }
+
+  console.log(row);
+}
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) console.log("Fizz");
+  else if (i % 5 === 0) console.log("Buzz");
+  else console.log(i);
+}
+  */
+ 
+
+
+
+
+
+
+
+
+
+
+
