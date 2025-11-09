c8b8b9e4b98c2e045eda704b3288b07b836773d3
naveen-sha <naveen972821@gmail.com>
2025-11-09 16:44:08 +0530

    ;learn about get/set and classes

--- javascript/love.js
+++ javascript/love.js
@@ -4,8 +4,14 @@ class human {
     // properties...
     name = "nikku";// public mark .
     #ht =80;// private not accsses outside the class.
-    wt=190;
+    wt;
+    age ;
     class ="12th";
+    // constructor its use  for inserting the value 
+    constructor(newage,newwt){
+        this.age=newage;
+        this.wt=newage;
+    }
     // behaviour...
     walking(){
         console.log("i am the knight king",this.#ht);
@@ -15,7 +21,19 @@ class human {
         console.log("i am the king ");
 
     }
+    // getter and setter 
+    // getter is to access the private value 
+    // setter is to update the private value
+    // its like function..
+    get fetchht() {
+        return this.#ht;
+    }
+    set modifyht(val){
+        return this.#ht=Val;
+    }
 }
-let obj =new human();
-console.log(obj.class);
+let obj =new human(17,66);
+ console.log(obj.fetchht);
 obj.walking();
+
+
